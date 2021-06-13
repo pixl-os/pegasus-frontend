2833e15f1c008027d70d2ddbeff081eda0738ee7
Bozo the geek <bozolesv2@gmail.com>
2021-06-13 23:58:05 +0200

    feat(retroachievements): process to manage arcade zip vs file zipped
    Still work under progress...

--- src/backend/providers/retroachievements/RetroAchievementsMetadata.cpp
+++ src/backend/providers/retroachievements/RetroAchievementsMetadata.cpp
@@ -9,9 +9,14 @@
 #include "model/gaming/Assets.h"
 #include "model/gaming/Collection.h"
 #include "model/gaming/Game.h"
+#include "model/gaming/GameFile.h"
 #include "providers/JsonCacheUtils.h"
 #include "providers/SearchContext.h"
 #include "utils/CommandTokenizer.h"
+#include "utils/Zip.h"
+
+#include "utils/rcheevos/include/rc_hash.h"
+
 //#include "utils/MoveOnly.h"
 
 #include <QJsonArray>
@@ -19,6 +24,7 @@
 #include <QJsonObject>
 #include <QNetworkReply>
 #include <QStringBuilder>
+#include <QDir>
 
 #include <QEventLoop>
 #include <QElapsedTimer>
@@ -27,6 +33,31 @@
 #include <RecalboxConf.h>
 
 namespace {
+QString PathMakeEscaped(QString param)
+{
+  std::string escaped = param.toUtf8().constData();
+
+  static std::string invalidChars = " '\"\\!$^&*(){}[]?;<>";
+  const char* invalids = invalidChars.c_str();
+  for(int i = escaped.size(); --i >= 0; )
+  {
+    char c = escaped.c_str()[i];
+    for(int j = invalidChars.size(); --j >= 0; )
+      if (c == invalids[j])
+      {
+        escaped.insert(i, "\\");
+        break;
+      }
+  }
+
+  return QString::fromStdString(escaped);
+}
+
+QString serialize_command(const QString& cmd, const QStringList& args)
+{
+    return (QStringList(QDir::toNativeSeparators(cmd)) + args).join(QLatin1String(" "));
+}
+
 QJsonDocument get_json_from_url(QString url_str, QString log_tag, QNetworkAccessManager &manager)
 {
 	QNetworkAccessManager* const manager_ptr = &manager;
@@ -180,7 +211,24 @@ QString get_token(QString log_tag, QString json_cache_dir, QNetworkAccessManager
 	return token;
 }	
 
-int get_gameid_from_hash(QString Hash, QString log_tag, QString json_cache_dir, QNetworkAccessManager &manager)
+QString calculate_hash_from_file(QString rom_file, QString log_tag)
+// can calculate hash using maner of RetroAchievements to manage the following system and format of rom supported by RetroAchievements.
+{
+	QElapsedTimer calculate_hash_timer;
+    calculate_hash_timer.start();
+	char hash_iterator[33] = "";
+	int result_iterator;
+	struct rc_hash_iterator iterator;
+	const char* path = rom_file.toLocal8Bit().data();	
+	rc_hash_initialize_iterator(&iterator, path, NULL, 0);
+	result_iterator = rc_hash_iterate(hash_iterator, &iterator);
+	rc_hash_destroy_iterator(&iterator);
+	Log::info(log_tag, LOGMSG("Stats - Timing: Hash processing: %1ms").arg(calculate_hash_timer.elapsed()));    
+	Log::debug(log_tag, LOGMSG("Hash on file: '%1' - '%2'").arg(rom_file, QString::fromLocal8Bit(hash_iterator)));
+	return QString::fromLocal8Bit(hash_iterator);
+}	
+
+int get_gameid_from_hash(QString Hash, QString log_tag, QNetworkAccessManager &manager)
 //from : http://retroachievements.org/dorequest.php?r=gameid&m=44dca16afbee7fd947e86c30c1183846
 {
 	QElapsedTimer get_gameid_timer;
@@ -356,17 +404,118 @@ void Metadata::fill_from_network(model::Game& game) const
 		if(game_ptr->RaGameID() == 0)
 		{
 			Log::debug(m_log_tag, LOGMSG("RetroAchievement RaGameId to find !"));
-			//calculate HASH
-			//TO DO
+			const model::GameFile* gamefile = game_ptr->filesConst().first(); /// take into account only the first file for the moment.
+			const QFileInfo& finfo = gamefile->fileinfo();		
+			//QString romfile = PathMakeEscaped(QDir::toNativeSeparators(finfo.absoluteFilePath()));
+			QString romfile = QDir::toNativeSeparators(finfo.absoluteFilePath());
+			QString targetfile;
+			//check if zip
+			if(romfile.toLower().endsWith(".zip"))
+			{	
+				Zip zip(Path(romfile.toLocal8Bit().data()));
+				Log::debug(m_log_tag, LOGMSG("This zip has %1 file(s).").arg(zip.Count()));
+				if(zip.Count() == 1)
+				{
+					//it seems a console game because only one file is present
+					//unzip file_unzipped
+					//example : unzip -o -d /tmp "/recalbox/share/roms/nes/Duck Hunt (World).zip"
+					QString UnzipCommand = "unzip";
+					QStringList args = QStringList {
+											QStringLiteral("-o"),
+											QStringLiteral("-d /tmp"),
+											"\""+romfile+"\""
+										};
+					int exitcode = system(qPrintable(serialize_command(UnzipCommand, args)));
+
+					//set target file from /tmp
+					targetfile = "/tmp/" + QString::fromStdString(zip.FileName(0).ToString());
+				}	
+				else	
+				{
+					//could be a arcade game in this case
+					//set target file from as the initial zip due to the fact that hash for arcade use the name of the file
+					targetfile = romfile;					
+				}
+			}
+			else
+			{	//not zipped
+				//set target file as the intial romfile
+				targetfile = romfile;	
+			}
+			Log::debug(m_log_tag, LOGMSG("The target file to hash is '%1'").arg(targetfile));
+			QString hash = calculate_hash_from_file(targetfile, m_log_tag);
+			
+			//check if tmp file used
+			if(targetfile.toLower().startsWith("/tmp/"))
+			{
+				Log::debug(m_log_tag, LOGMSG("Deletion of target file : '%1'").arg(targetfile));
+				//delete file
+				QString DeleteFileCommand = "rm";
+				QStringList args = QStringList {"\""+targetfile+"\""};
+				int exitcode = system(qPrintable(serialize_command(DeleteFileCommand, args)));
+			}
+			
+			//FOR TEST ONLY
+			//QString file_unzipped = "/recalbox/share/roms/nes/Duck Hunt (World).nes";
+			//Log::debug(m_log_tag, LOGMSG("Rom Filepath to hash: '%1'").arg(file_unzipped));
+			//const char* path = file_unzipped.toLocal8Bit().data();		
+
+
+/* 			//methods without console_id to provide (better)
+			char hash_iterator[33] = "";
+			int result_iterator;
+			struct rc_hash_iterator iterator;
+			
+			//if zip, if one file only, unzip file in buffer and calculate hash
+			QString file_zipped = finfo.absoluteFilePath();
+			Zip zip(Path(file_zipped.toLocal8Bit().data()));
+			Log::debug(m_log_tag, LOGMSG("This zip has %1 file(s).").arg(zip.Count()));
+			if(zip.Count() == 1)
+			{
+				Log::debug(m_log_tag, LOGMSG("The file in zip is named : %1").arg(zip.FileName(0));
+				Log::debug(m_log_tag, LOGMSG("The file in zip has as size of %1 octet(s).").arg(zip.UncompressedSize(0)));
+				const char* tmp_path = file_unzipped.toLocal8Bit().data();	
+				rc_hash_initialize_iterator(&iterator, tmp_path, NULL, 0);
+				result_iterator = rc_hash_iterate(hash_iterator, &iterator);
+				rc_hash_destroy_iterator(&iterator);
+				Log::debug(m_log_tag, LOGMSG("Hash of file (using iterator and file unzipped): '%1'").arg(QString::fromLocal8Bit(hash_iterator)));
+			}
+
+			//methogs with console_id to provide (for complex)
+			char hash_file[33] = "";
+			int console_id = RC_CONSOLE_NINTENDO;
+			const char* path2 = file_unzipped.toLocal8Bit().data();
+			int result_file = rc_hash_generate_from_file(hash_file, console_id, path2);
+			Log::debug(m_log_tag, LOGMSG("Hash of file (using console_id): '%1'").arg(QString::fromLocal8Bit(hash_file)));
+
+			//for test: methods with zip extension as for arcade games (neogeo, fbneo, mame, atomiswave, naomi, naomigd, etc...)	
+			char hash_iterator3[33] = "";
+			int result_iterator3;
+			struct rc_hash_iterator iterator3;
+			const char* path3 = file_zipped.toLocal8Bit().data();
+			rc_hash_initialize_iterator(&iterator3, path3, NULL, 0);
+			result_iterator3 = rc_hash_iterate(hash_iterator3, &iterator3);
+			rc_hash_destroy_iterator(&iterator3);
+			Log::debug(m_log_tag, LOGMSG("Hash of file (using iterator and zip format as for arcade games): '%1'").arg(QString::fromLocal8Bit(hash_iterator3)));
+ */
+			
 			//TEST HASH of Nes - Duck Hunt
-			QString hash = "fa382374eb4a93a719064ca6c5a4e78c";
-			game_ptr->setRaGameID(get_gameid_from_hash(hash,m_log_tag, m_json_cache_dir, *manager));
+			//QString hash = "fa382374eb4a93a719064ca6c5a4e78c";
+			//Log::debug(m_log_tag, LOGMSG("Duck Hunt (World) - NES - hash value from RA : '%1'").arg(hash));
+			
+			game_ptr->setRaGameID(get_gameid_from_hash(hash,m_log_tag, *manager));
 			Log::debug(m_log_tag, LOGMSG("RetroAchievement GameId found is : %1").arg(game_ptr->RaGameID()));
+			
+
 		}
 		else
 		{
 			Log::debug(m_log_tag, LOGMSG("RetroAchievement GameId already known : %1").arg(game_ptr->RaGameID()));
 		}
+		
+		//for test purpose / we reset gameid to be able to retest just after ;-)
+		game_ptr->setRaGameID(0);
+		
 	}
 	else return;
 	
