02753c90f1c3067a11fbed55f26f4970d2d19921
Bozo the geek <bozolesv2@gmail.com>
2024-03-10 15:46:33 +0100

    feat(HttpServer): introduce HTTP API to interact between scripts and pegasus-frontend
    feat(audiovolume): implement first possibility to dynamically update menu options using biding
    
    done for "audio.volume" parameters to experiment how to do to use biding
    
    fix(audiovolume): remove test/dev line

--- src/backend/model/internal/settings/Recalbox.h
+++ src/backend/model/internal/settings/Recalbox.h
@@ -16,26 +16,34 @@ namespace model {
 /// Provides a recalbox.conf interface for the frontend layer
 class Recalbox : public QObject {
     Q_OBJECT
-    QML_CONST_PROPERTY(model::ParametersList, parameterslist)
-    
+
+    //for recalbox.conf parameters using binding to be updated dynamically in menu and from other tools/services/scripts
+    Q_PROPERTY(int audioVolume READ getAudioVolume WRITE setAudioVolume NOTIFY audioVolumeChanged);
+
+    QML_CONST_PROPERTY(model::ParametersList, parameterslist);
+
 public:
     explicit Recalbox(QObject* parent = nullptr);
-   
+
+    //for recalbox.conf parameters using binding to be updated dynamically in menu and from other tools/services/scripts
+    int getAudioVolume() const { return RecalboxConf::Instance().GetAudioVolume(); }
+    void setAudioVolume(int);
+
+    //INVOKABLE functions but without binding
     Q_INVOKABLE QString getStringParameter(const QString& Parameter, const QString& defaultValue = "");
     Q_INVOKABLE void setStringParameter(const QString& Parameter, const QString& Value);
-    
     Q_INVOKABLE bool getBoolParameter(const QString& Parameter, const bool& defaultValue = false);
     Q_INVOKABLE void setBoolParameter(const QString& Parameter, const bool& Value);
-
     Q_INVOKABLE int getIntParameter(const QString& Parameter);
     Q_INVOKABLE void setIntParameter(const QString& Parameter, const int& Value);
-       
     Q_INVOKABLE void saveParameters();
-    
-    Q_INVOKABLE void reloadParameters(); //to relaod parameters from recalbox.conf
-
+    Q_INVOKABLE void reloadParameter(QString parameters); //to reload any parameters from recalbox.conf
     Q_INVOKABLE QString runCommand(const QString& SysCommand, const QStringList& SysOptions);
-    
+
+signals:
+    //for recalbox.conf parameters using binding to be updated dynamically in menu and from other tools/services/scripts
+    void audioVolumeChanged();
+
 private:
 
     //! Boot configuration file
